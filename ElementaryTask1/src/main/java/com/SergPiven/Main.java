e657d688a176e3b098c75f9eccca4b9e916bce2a
SergPiven <sergeypiven2101@gmail.com>
2019-10-11 08:53:50 +0300

    azxc

--- ElementaryTask1/src/main/java/com/SergPiven/Main.java
+++ ElementaryTask1/src/main/java/com/SergPiven/Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Initiliazition.run(args 5,5);
+        Initiliazition.run(args 10,10);
     }
 }
